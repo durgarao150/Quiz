391a7f45d53a55b919a0efc06e17c3ea89da6f5d
S Durga Prasad Rao <durgaprasadrao50@gmail.com>
2024-10-25 00:51:36 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,46 @@
+// index.js
+
+var total = 10; // Number of total questions
+
+var questions = [
+    "",
+    "What is the largest planet in our Solar System?",
+    "Which planet is known as the 'Red Planet'?",
+    "How long does it take for the Earth to complete one orbit around the Sun?",
+    "What is the hottest planet in our solar system?",
+    "Which planet has the most moons?",
+    "What is the name of the first artificial satellite launched into space?",
+    "Which planet is famous for its rings?",
+    "Which star is the closest to Earth?",
+    "What is the name of the galaxy we live in?",
+    "What is the term for a rocky object that burns up upon entering Earth's atmosphere?"
+];
+
+var choices = [
+    [],
+    ["Earth", "Mars", "Jupiter", "Saturn"],
+    ["Venus", "Mars", "Mercury", "Neptune"],
+    ["24 hours", "365 days", "30 days", "12 months"],
+    ["Venus", "Mercury", "Mars", "Earth"],
+    ["Jupiter", "Saturn", "Neptune", "Uranus"],
+    ["Apollo 11", "Voyager 1", "Sputnik 1", "Hubble"],
+    ["Uranus", "Neptune", "Saturn", "Mars"],
+    ["Proxima Centauri", "Alpha Centauri", "The Sun", "Sirius"],
+    ["Andromeda", "Milky Way", "Whirlpool", "Sombrero"],
+    ["Asteroid", "Meteor", "Comet", "Meteorite"]
+];
+
+var solutions = ["", "c", "b", "b", "a", "b", "c", "c", "c", "b", "b"]; // Correct answers
+
+var compliments = [
+    "Awesome job!",
+    "You're doing fantastic!",
+    "Brilliant work!",
+    "You’re crushing it!",
+    "Super impressive!",
+    "You’ve got this!",
+    "Great effort, keep it up!",
+    "Nicely done!",
+    "That was spot on!",
+    "You're on a roll!"
+];
